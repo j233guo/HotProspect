8424486823629e5892050191545fbcd4eeba8cde
Jiaming Guo <j23guo@gmail.com>
2022-10-19 23:45:23 -0400

    Initial Commit

---
+++ HotProspect/HotProspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectApp.swift
+//  HotProspect
+//
+//  Created by Jiaming Guo on 2022-10-19.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
