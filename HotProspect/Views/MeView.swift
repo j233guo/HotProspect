38be3ab1839f28c8696daf277c71fe2fe5b6b5ad
Jiaming Guo <j23guo@gmail.com>
2022-11-08 20:50:21 -0500

    integrated qr code functions

--- HotProspect/Views/MeView.swift
+++ HotProspect/Views/MeView.swift
@@ -5,13 +5,42 @@
 //  Created by Jiaming Guo on 2022-11-06.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "a@b.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
     var body: some View {
         NavigationView {
-            Text("Me")
-                .navigationTitle("Me")
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                TextField("Email Address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+            }
+            .navigationTitle("Your Code")
         }
     }
 }
--- HotProspect/Views/ProspectsView.swift
+++ HotProspect/Views/ProspectsView.swift
@@ -5,16 +5,18 @@
 //  Created by Jiaming Guo on 2022-11-06.
 //
 
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
     
     enum FilterType {
         case none, contacted, uncontacted
     }
-    
     let filter: FilterType
+    
     var title: String {
         switch filter {
             case .none:
@@ -26,20 +28,54 @@ struct ProspectsView: View {
         }
     }
     
+    var filteredProspects: [Prospect] {
+        switch filter {
+            case .none:
+                return prospects.people
+            case .contacted:
+                return prospects.people.filter { $0.isContacted }
+            case .uncontacted:
+                return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        switch result {
+            case .success(let result):
+                let details = result.string.components(separatedBy: "\n")
+                guard details.count == 2 else { return }
+                let person = Prospect()
+                person.name = details[0]
+                person.emailAddress = details[1]
+            case .failure(let error):
+                print(error.localizedDescription)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            Text("People: \(prospects.people.count)")
-                .navigationTitle(title)
-                .toolbar {
-                    Button {
-                        let prospect = Prospect()
-                        prospect.name = "someone"
-                        prospect.emailAddress = "a@b.com"
-                        prospects.people.append(prospect)
-                    } label: {
-                        Label("Scan", systemImage: "qrcode.viewfinder")
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
                     }
                 }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button {
+                    isShowingScanner = true
+                } label: {
+                    Label("Scan", systemImage: "qrcode.viewfinder")
+                }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "jg", completion: handleScan)
+            }
         }
     }
 }
